fb8f78b27d87be78e9acee5c31e66cec7aa76e96
J.M. Cléry <jmclery@gmail.com>
2023-01-26 15:27:30 +0100

    add: todolist almost finished

--- J4/todolist/src/components/Todoitem.jsx
+++ J4/todolist/src/components/Todoitem.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function Todoitem(props) {
+  // (exemple)
+  // props.task =  { id: uuidv4(), title: 'Apprendre JS', isCompleted: true }
+  // props.updateTask =  function updateTask(updatedTask) { … }
+
+  function editTask() {
+    const task = {
+      ...props.task,
+      isCompleted : !props.task.isCompleted
+    };
+    props.updateTask(task);
+  }
+
   return (
     <div>
-      <input type="checkbox" checked={props.task.isCompleted} />
+      <input type="checkbox" checked={props.task.isCompleted} onChange={editTask} />
       <span>{props.task.title}</span>
     </div>
   );
