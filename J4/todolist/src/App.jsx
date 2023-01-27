c50395019a31ed0b8da76334f46983281f70b719
J.M. Cléry <jmclery@gmail.com>
2023-01-27 10:11:13 +0100

    add: todolist final

--- J4/todolist/src/App.jsx
+++ J4/todolist/src/App.jsx
@@ -1,36 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Todolist from './components/Todolist'
+import { v4 as uuidv4 } from "uuid";
+import TodoForm from './components/TodoForm';
 
 function App() {
 
-  /*
-    [
-      {
-        title: 'Liste pour les courses',
-        tasks : [
-                { id: uuidv4(), title: 'Apprendre JS', isCompleted: true },
-                { id: uuidv4(), title: 'Apprendre CSS', isCompleted: true },
-                { id: uuidv4(), title: 'Apprendre React', isCompleted: false },
-              ]
-      }
-      ,
+  const [todolists, setTodolists] = useState([
+    {
+      id: uuidv4(),
+      title: 'Cours',
+      tasks: [
+        { id: uuidv4(), title: 'Apprendre JS', isCompleted: true },
+        { id: uuidv4(), title: 'Apprendre CSS', isCompleted: true },
+        { id: uuidv4(), title: 'Apprendre HTML', isCompleted: true },
+        { id: uuidv4(), title: 'Apprendre React', isCompleted: false },
+        { id: uuidv4(), title: 'Apprendre Redux', isCompleted: false },
+      ]
+    },
+    {
+      id: uuidv4(),
+      title: 'Liste de courses',
+      tasks: [
+        { id: uuidv4(), title: 'Acheter du pain', isCompleted: true },
+        { id: uuidv4(), title: 'Nourrir le chat', isCompleted: true },
+      ]
+    },
+  ]);
 
-      {
-        title: 'Liste pour les courses',
-        tasks : [
-                { id: uuidv4(), title: 'Apprendre JS', isCompleted: true },
-                { id: uuidv4(), title: 'Apprendre CSS', isCompleted: true },
-                { id: uuidv4(), title: 'Apprendre React', isCompleted: false },
-              ]
-      }
-    ]
-  */
+  function addTodolist(title) {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      tasks: []
+    };
+    setTodolists([...todolists, newTodo]);
+  }
+
+  function deleteList(id) {
+    const listIndex = todolists.findIndex(list => list.id === id);
+    todolists.splice(listIndex, 1);
+    setTodolists([...todolists]);
+  }
 
   return (
     <div className="container">
       <h1>Application de gestion de tâches</h1>
       <hr />
-      <Todolist />
+      <TodoForm onTask={addTodolist} />
+      {todolists.map(todolist => (
+        <Todolist key={todolist.id} tasks={todolist.tasks} title={todolist.title} listId={todolist.id} onDeleteList={deleteList} />
+      ))}
     </div>
   )
 }
